f1865619d33c9b53a507e816022a077aff8ae3da
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-12 08:55:02 +0300

    ISeriesRelationalModel reduced

--- src/Pure.Chart.RelationalModel.Abstractions/ISeriesRelationalModel.cs
+++ src/Pure.Chart.RelationalModel.Abstractions/ISeriesRelationalModel.cs
@@ -1,11 +1,17 @@
-using Pure.Chart.Model.Abstractions;
 using Pure.Primitives.Abstractions.Guid;
+using Pure.Primitives.Abstractions.String;
 
 namespace Pure.Chart.RelationalModel.Abstractions;
 
-public interface ISeriesRelationalModel : ISeries
+public interface ISeriesRelationalModel
 {
     public IGuid Id { get; }
 
     public IGuid ChartId { get; }
+
+    public IString Legend { get; }
+
+    public IString XAxisSource { get; }
+
+    public IString YAxisSource { get; }
 }
