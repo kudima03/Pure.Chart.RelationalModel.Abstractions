151b7f91b619d0a4d0ee131936cced866237c2bf
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-12 08:55:11 +0300

    IChartRelationalModel reduced

--- src/Pure.Chart.RelationalModel.Abstractions/IChartRelationalModel.cs
+++ src/Pure.Chart.RelationalModel.Abstractions/IChartRelationalModel.cs
@@ -1,11 +1,17 @@
-using Pure.Chart.Model.Abstractions;
 using Pure.Primitives.Abstractions.Guid;
+using Pure.Primitives.Abstractions.String;
 
 namespace Pure.Chart.RelationalModel.Abstractions;
 
-public interface IChartRelationalModel : IChart
+public interface IChartRelationalModel
 {
     public IGuid Id { get; }
+
+    public IString Title { get; }
+
+    public IString Description { get; }
+
+    public IGuid TypeId { get; }
 
     public IGuid XAxisId { get; }
 
